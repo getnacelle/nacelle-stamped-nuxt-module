f09db347fe1a3f1625955e61589b11ebd0262e27
Nick Richmond <nwrichmond@turnt.tech>
2019-12-20 15:57:42 -0500

    Load the Stamped script in the plugin

--- lib/components/StampedMainWidget.vue
+++ lib/components/StampedMainWidget.vue
@@ -38,10 +38,10 @@ export default {
   },
   computed: {
     ...mapState('space', ['domain']),
-    productUrl () {
+    productUrl() {
       return `${this.domain}/products/${this.product.handle}`
     },
-    decodedProductId () {
+    decodedProductId() {
       if (this.product && this.product.id) {
         const decodedIdUri = window.atob(this.product.id)
         const decodedIdParts = decodedIdUri.split('Product/')
@@ -51,36 +51,9 @@ export default {
 
       return ''
     }
-  },
-  mounted () {
-    window.stampedLoaded = window.stampedLoaded || false
-
-    if (!window.stampedLoaded) {
-      // spoof Shopify global var
-      window.Shopify = {}
-      window.Shopify.shop = this.$nacelle.stamped.domain
-
-      // load Stamped script
-      const s = document.createElement('script')
-      s.type = 'text/javascript'
-      s.src = 'https://cdn-stamped-io.azureedge.net/files/widget.min.js'
-      s.setAttribute('data-api-key', this.$nacelle.stamped.apiKey)
-      document.body.appendChild(s)
-
-      window.stampedLoaded = true
-    } else if (window.StampedFn) {
-      window.StampedFn.init()
-    } else {
-      const waiting = setInterval(() => {
-        if (window.StampedFn) {
-          clearInterval(waiting)
-        }
-      }, 100)
-    }
   }
 }
 </script>
 
 <style>
-
 </style>
